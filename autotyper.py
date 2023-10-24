7aa9e8f200b4dee5a0f253b372a066dc8674d795
Kota Amemiya <kotaamemiya@gmail.com>
2023-10-24 17:48:09 +0100

    Create function to run only when ctrl is pressed.

--- autotyper.py
+++ autotyper.py
@@ -1,11 +1,44 @@
 import time
+from pynput import keyboard
 from pynput.keyboard import Key, Controller
 
 my_keyboard = Controller()
+def cmdv(): #to test
+    with my_keyboard.pressed(Key.cmd):
+        my_keyboard.type('v')
+def left():
+    my_keyboard.press(Key.left)
+    time.sleep(0.1)
+    my_keyboard.release(Key.left)
+def right():
+    my_keyboard.press(Key.right)
+    time.sleep(0.1)
+    my_keyboard.release(Key.right)
 
-time.sleep(2)
+running = False
 
-text = "hello"
-for c in text:
-    my_keyboard.type(c)
-    time.sleep(0.001)
+def on_press(key):
+    global running
+    if key == Key.ctrl and not running:
+        running = True
+        while running:
+            left()
+            right()
+
+def on_release(key):
+    global running
+    if key == Key.ctrl and running:
+        running = False
+
+
+senser = keyboard.Listener(on_press=on_press, on_release=on_release)
+senser.start()
+time.sleep(10)
+senser.stop()
+
+#ADD WHAT YOU WANT TO AUTOTYPE INTO TEXT
+# time.sleep(5)
+# text = "Replace"
+# for c in text:
+#     my_keyboard.type(c)
+#     time.sleep(0.1) #If system doesn't load your full text, up the number
